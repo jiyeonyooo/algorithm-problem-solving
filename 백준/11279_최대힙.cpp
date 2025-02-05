fd6cf0f5ea6168455b91c1cc4125f2835b94fbb8
jiyeonyooo <ji04230423@gmail.com>
2025-02-05 19:21:22 +0900

    250205 최대 힙

---
+++ 백준/11279_최대힙.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> primes; //min heap
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k, n;        //소수의 개수, n번째 값
+    int prime;
+    int cnt = 0;
+
+    vector<int> p; //입력 소수 저장 배열
+
+    cin >> k >> n;
+
+    for (int i = 0; i < k; i++) {
+        cin >> prime;
+        p.push_back(prime);
+        primes.push(prime);
+    }
+
+    long long now = 0;
+
+    while (cnt < n) {
+
+        if (now == primes.top()) {
+            primes.pop();
+        }
+        else {
+            now = primes.top();
+            primes.pop();
+            cnt++;
+
+            for (int j = 0; j < k; j++) { //중복이 아닌 경우만 수행
+                if (now * p[j] >= 2147483647) continue;
+                else primes.push(now * p[j]);
+            }
+        }
+    }
+
+    cout << now;
+
+    return 0;
+}
